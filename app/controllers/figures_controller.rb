eb3914651d33a733ab4d625435f82ec8aed3b751
James Ferguson <ncjamesferguson@gmail.com>
2018-03-03 01:08:26 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,14 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    @figures = Figure.all
+    @landmarks = Landmark.all
+    @titles = Title.all
+
+    erb :'/figures/index'
+  end
+
+
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
